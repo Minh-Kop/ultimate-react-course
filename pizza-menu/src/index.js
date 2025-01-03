11ee7d72adaa15a2868b5576ff75d7380b07e60e
Minh Kop <87182623+Minh-Kop@users.noreply.github.com>
2025-01-03 08:31:56 +0700

    47. Conditional Rendering With &&

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -69,13 +69,18 @@ function Header() {
 }
 
 function Menu() {
+  const pizzas = pizzaData;
+  // const pizzas = [];
+  const numPizzas = pizzas.length;
+
   return (
     <main className="menu">
       <h2>Our menu</h2>
       <ul className="pizzas">
-        {pizzaData.map((pizza) => {
-          return <Pizza pizzaObj={pizza} key={pizza.name} />;
-        })}
+        {numPizzas > 0 &&
+          pizzas.map((pizza) => {
+            return <Pizza pizzaObj={pizza} key={pizza.name} />;
+          })}
       </ul>
 
       {/* <Pizza
@@ -110,14 +115,19 @@ function Pizza(props) {
 
 function Footer() {
   const hour = new Date().getHours();
-  const openHour = 12;
+  const openHour = 8;
   const closeHour = 24;
   const isOpen = hour >= openHour && hour < closeHour;
   console.log(isOpen);
 
   return (
     <footer className="footer">
-      {new Date().toLocaleTimeString()} We're currently open!
+      {isOpen && (
+        <div className="order">
+          <p>We're open until {closeHour}:00. Come visit us or order online</p>
+          <button className="btn">order</button>
+        </div>
+      )}
     </footer>
   );
 }
