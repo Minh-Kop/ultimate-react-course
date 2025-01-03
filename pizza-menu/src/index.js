e5568d0d747ad42aa3d840b938014565ab9f1989
Minh Kop <87182623+Minh-Kop@users.noreply.github.com>
2025-01-03 11:29:51 +0700

    50. Extracting JSX Into a New Component

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -123,7 +123,7 @@ function Pizza(props) {
 
 function Footer() {
   const hour = new Date().getHours();
-  const openHour = 12;
+  const openHour = 11;
   const closeHour = 24;
   const isOpen = hour >= openHour && hour < closeHour;
   console.log(isOpen);
@@ -131,16 +131,24 @@ function Footer() {
   return (
     <footer className="footer">
       {isOpen ? (
-        <div className="order">
-          <p>We're open until {closeHour}:00. Come visit us or order online.</p>
-          <button className="btn">order</button>
-        </div>
+        <Order closeHour={closeHour} />
       ) : (
         <p>
           We're happy to welcome you between {openHour}:00 and {closeHour}:00.
         </p>
       )}
     </footer>
+  );
+}
+
+function Order(props) {
+  return (
+    <div className="order">
+      <p>
+        We're open until {props.closeHour}:00. Come visit us or order online.
+      </p>
+      <button className="btn">order</button>
+    </div>
   );
 }
 
