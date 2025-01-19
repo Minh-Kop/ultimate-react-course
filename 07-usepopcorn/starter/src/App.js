5244d58ee64fe727fc0b4af5d9f20bb59936c317
Minh Kop <87182623+Minh-Kop@users.noreply.github.com>
2025-01-19 11:42:12 +0700

    146. Adding a Loading State

--- 07-usepopcorn/starter/src/App.js
+++ 07-usepopcorn/starter/src/App.js
@@ -55,16 +55,18 @@ const KEY = 'ccc856c3';
 export default function App() {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const query = 'batman';
 
   useEffect(function () {
     async function fecthMovies() {
+      setIsLoading(true);
       const res = await fetch(
         `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
       );
       const data = await res.json();
       setMovies(data.Search);
-      console.log(data.Search);
+      setIsLoading(false);
     }
     fecthMovies();
   }, []);
@@ -77,9 +79,7 @@ export default function App() {
       </NavBar>
 
       <Main>
-        <Box>
-          <MovieList movies={movies} />
-        </Box>
+        <Box>{isLoading ? <Loader /> : <MovieList movies={movies} />}</Box>
 
         <Box>
           <WatchedSummary watched={watched} />
@@ -88,6 +88,10 @@ export default function App() {
       </Main>
     </>
   );
+}
+
+function Loader() {
+  return <div className="loader">Loading...</div>;
 }
 
 function NavBar({ children }) {
