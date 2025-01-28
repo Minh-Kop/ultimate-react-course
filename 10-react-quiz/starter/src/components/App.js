5cb0ac10e73f8fceebc7a99030efb5372743e172
Minh Kop <87182623+Minh-Kop@users.noreply.github.com>
2025-01-28 15:47:31 +0700

    195. Handling New Answers

--- 10-react-quiz/starter/src/components/App.js
+++ 10-react-quiz/starter/src/components/App.js
@@ -11,6 +11,8 @@ const initialState = {
   // 'loading', 'error', 'ready', 'active', 'finished'
   status: 'loading',
   index: 0,
+  answer: null,
+  points: 0,
 };
 
 function reducer(state, action) {
@@ -25,13 +27,25 @@ function reducer(state, action) {
       return { ...state, status: 'error' };
     case 'start':
       return { ...state, status: 'active' };
+    case 'newAnswer':
+      const question = state.questions[state.index];
+      const answer = action.payload;
+
+      return {
+        ...state,
+        answer,
+        points:
+          answer === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
     default:
       throw new Error('Action unknown');
   }
 }
 
 export default function App() {
-  const [{ questions, status, index }, dispatch] = useReducer(
+  const [{ questions, status, index, answer }, dispatch] = useReducer(
     reducer,
     initialState
   );
@@ -55,7 +69,13 @@ export default function App() {
         {status === 'ready' && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === 'active' && <Question question={questions[index]} />}
+        {status === 'active' && (
+          <Question
+            question={questions[index]}
+            dispatch={dispatch}
+            answer={answer}
+          />
+        )}
       </Main>
     </div>
   );
