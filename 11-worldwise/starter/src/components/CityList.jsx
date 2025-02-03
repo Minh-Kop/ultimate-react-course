5ac410e4c92d83687ac275b32c83844a57677f61
Minh Kop <87182623+Minh-Kop@users.noreply.github.com>
2025-02-03 17:36:41 +0700

    228. Consuming the CitiesContext

--- 11-worldwise/starter/src/components/CityList.jsx
+++ 11-worldwise/starter/src/components/CityList.jsx
@@ -1,15 +1,20 @@
-import CityItem from './CityItem';
-import styles from './CityList.module.css';
-import Spinner from './Spinner';
-import Message from './Message';
+import styles from "./CityList.module.css";
+import CityItem from "./CityItem";
+import Spinner from "./Spinner";
+import Message from "./Message";
+import { useCities } from "../contexts/CitiesContext";
 
-function CityList({ cities, isLoading }) {
+function CityList() {
+  const { cities, isLoading } = useCities();
+
   if (isLoading) {
     return <Spinner />;
   }
 
   if (!cities.length) {
-    return <Message message='Add your first city by clicking on a city on the map' />;
+    return (
+      <Message message="Add your first city by clicking on a city on the map" />
+    );
   }
 
   return (
