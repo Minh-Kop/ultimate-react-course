58d936cf4170c0c2bc2e48128816a2894b2e91dc
Minh Kop <87182623+Minh-Kop@users.noreply.github.com>
2025-02-03 18:33:01 +0700

    229. Finishing the City View

--- 11-worldwise/starter/src/components/CityItem.jsx
+++ 11-worldwise/starter/src/components/CityItem.jsx
@@ -1,21 +1,25 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-import styles from './CityItem.module.css';
+import styles from "./CityItem.module.css";
+import { useCities } from "../contexts/CitiesContext";
 
 const formatDate = (date) =>
-  new Intl.DateTimeFormat('en', {
-    day: 'numeric',
-    month: 'long',
-    year: 'numeric',
+  new Intl.DateTimeFormat("en", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
   }).format(new Date(date));
 
 function CityItem({ city }) {
+  const { currentCity } = useCities();
   const { cityName, emoji, date, id, position } = city;
 
   return (
     <li>
       <Link
-        className={styles.cityItem}
+        className={`${styles.cityItem} ${
+          id === currentCity.id ? styles["cityItem--active"] : ""
+        }`}
         to={`${id}?lat=${position.lat}&lng=${position.lng}`}
       >
         <span className={styles.emoji}>{emoji}</span>
