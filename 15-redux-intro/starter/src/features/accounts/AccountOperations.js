185a1429d26228d3bb96205ada315f27af8c4ab5
Minh Kop <87182623+Minh-Kop@users.noreply.github.com>
2025-02-11 07:49:31 +0700

    271. Making an API Call With Redux Thunks

--- 15-redux-intro/starter/src/features/accounts/AccountOperations.js
+++ 15-redux-intro/starter/src/features/accounts/AccountOperations.js
@@ -15,16 +15,16 @@ function AccountOperations() {
     loan: currentLoan,
     loanPurpose: currentLoanPurpose,
     balance,
+    isLoading,
   } = useSelector((store) => store.account);
 
-  console.log(balance);
-
   function handleDeposit() {
-    if (!depositAmount) {
+    if (!depositAmount || !currency) {
       return;
     }
-    dispatch(deposit(depositAmount));
+    dispatch(deposit(depositAmount, currency));
     setDepositAmount("");
+    setCurrency("");
   }
 
   function handleWithdrawal() {
@@ -68,7 +68,9 @@ function AccountOperations() {
             <option value="GBP">British Pound</option>
           </select>
 
-          <button onClick={handleDeposit}>Deposit {depositAmount}</button>
+          <button onClick={handleDeposit} disabled={isLoading}>
+            {isLoading ? "Converting..." : `Deposit ${depositAmount}`}
+          </button>
         </div>
 
         <div>
