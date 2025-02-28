e36aa8660a143abfde712892923e2c352b70499a
Minh Kop <87182623+Minh-Kop@users.noreply.github.com>
2025-02-28 09:52:46 +0700

    335. Building the Sidebar and Main Navigation

--- 17-the-wild-oasis/starter/src/App.jsx
+++ 17-the-wild-oasis/starter/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import GlobalStyles from "./styles/GlobalStyles";
 import Dashboard from "./pages/Dashboard";
 import Bookings from "./pages/Bookings";
-import Cabins from "./pages/Dashboard";
+import Cabins from "./pages/Cabins";
 import Users from "./pages/Users";
 import Settings from "./pages/Settings";
 import Account from "./pages/Account";
