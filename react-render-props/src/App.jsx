b68ca5bfc62f6bfc14af895431eedd6af89afce9
Minh Kop <87182623+Minh-Kop@users.noreply.github.com>
2025-03-07 17:16:36 +0700

    365. A Look at Higher-Order Components (HOC)

--- react-render-props/src/App.jsx
+++ react-render-props/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { faker } from "@faker-js/faker";
 
 import "./styles.css";
+import withToggles from "./HOC";
 
 const products = Array.from({ length: 20 }, () => {
   return {
@@ -75,6 +76,19 @@ function List({ title, items, render }) {
   );
 }
 
+// LATER: Let's say we got this component from a 3rd-party library, and can't change it. But we still want to add the 2 toggle functionalities to it
+function ProductList({ title, items }) {
+  return (
+    <ul className="list">
+      {items.map((product) => (
+        <ProductItem key={product.productName} product={product} />
+      ))}
+    </ul>
+  );
+}
+
+const ProductListWithToggles = withToggles(ProductList);
+
 export default function App() {
   return (
     <div>
@@ -100,17 +114,11 @@ export default function App() {
           )}
         />
       </div>
-    </div>
-  );
-}
 
-// LATER: Let's say we got this component from a 3rd-party library, and can't change it. But we still want to add the 2 toggle functionalities to it
-function ProductList({ title, items }) {
-  return (
-    <ul className="list">
-      {items.map((product) => (
-        <ProductItem key={product.productName} product={product} />
-      ))}
-    </ul>
+      <div className="col-2">
+        <ProductList title="Products HOC" items={products} />
+        <ProductListWithToggles title="Products HOC" items={products} />
+      </div>
+    </div>
   );
 }
