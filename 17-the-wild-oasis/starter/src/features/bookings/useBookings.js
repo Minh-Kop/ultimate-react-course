67f1706384bf0568746fd47432f18958482a8721
Minh Kop <87182623+Minh-Kop@users.noreply.github.com>
2025-03-11 23:41:12 +0700

    380. API-Side Sorting: Sorting Bookings

--- 17-the-wild-oasis/starter/src/features/bookings/useBookings.js
+++ 17-the-wild-oasis/starter/src/features/bookings/useBookings.js
@@ -11,15 +11,20 @@ export function useBookings() {
   const filter =
     !filterValue || filterValue === "all"
       ? null
-      : { field: "status", value: filterValue };
+      : { field: "status", value: filterValue, method: "eq" };
+
+  // SORT
+  const sortByRaw = searchParams.get("sortBy") || "startDate-desc";
+  const [field, direction] = sortByRaw.split("-");
+  const sortBy = { field, direction };
 
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ["bookings", filter],
-    queryFn: () => getBookings({ filter }),
+    queryKey: ["bookings", filter, sortBy],
+    queryFn: () => getBookings({ filter, sortBy }),
   });
 
   return { isLoading, bookings, error };
