6cbbb6669aff160a56de0ee02ec82f7e63f5da40
Minh Kop <87182623+Minh-Kop@users.noreply.github.com>
2025-03-11 16:17:17 +0700

    376. Client-Side Sorting: Sorting Cabins

--- 17-the-wild-oasis/starter/src/features/cabins/CabinTable.jsx
+++ 17-the-wild-oasis/starter/src/features/cabins/CabinTable.jsx
@@ -14,6 +14,7 @@ function CabinTable() {
     return <Spinner />;
   }
 
+  // 1) FILTER
   const filterValue = searchParams.get("discount") || "all";
 
   let filteredCabins;
@@ -24,6 +25,14 @@ function CabinTable() {
   } else if (filterValue === "with-discount") {
     filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
   }
+
+  // 2) SORT
+  const sortBy = searchParams.get("sortBy") || "startDate-asc";
+  const [field, direction] = sortBy.split("-");
+  const modifier = direction === "asc" ? 1 : -1;
+  const sortedCabins = filteredCabins.sort(
+    (a, b) => (a[field] - b[field]) * modifier
+  );
 
   return (
     <Menus>
@@ -38,7 +47,7 @@ function CabinTable() {
         </Table.Header>
 
         <Table.Body
-          data={filteredCabins}
+          data={sortedCabins}
           render={(cabin) => <CabinRow key={cabin.id} cabin={cabin} />}
         />
       </Table>
