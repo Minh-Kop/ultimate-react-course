64bf15cc31effbd583e71dd9c8511e1ce4f532fa
Minh Kop <87182623+Minh-Kop@users.noreply.github.com>
2025-03-15 20:52:42 +0700

    390. Authorization: Protecting Routes

--- 17-the-wild-oasis/starter/src/features/authentication/useLogin.js
+++ 17-the-wild-oasis/starter/src/features/authentication/useLogin.js
@@ -1,15 +1,17 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-hot-toast";
 
 import { login as loginApi } from "../../services/apiAuth";
 
 export function useLogin() {
+  const queryClient = useQueryClient();
   const navigate = useNavigate();
 
   const { mutate: login, isPending } = useMutation({
     mutationFn: loginApi,
     onSuccess: (user) => {
+      queryClient.setQueriesData(["user"], user);
       navigate("/dashboard");
     },
     onError: (err) => {
