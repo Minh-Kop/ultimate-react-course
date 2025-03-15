110eaa163737e976482089e3332f4a37c786329b
Minh Kop <87182623+Minh-Kop@users.noreply.github.com>
2025-03-15 23:11:54 +0700

    391. User Logout

--- 17-the-wild-oasis/starter/src/features/authentication/useLogin.js
+++ 17-the-wild-oasis/starter/src/features/authentication/useLogin.js
@@ -12,7 +12,7 @@ export function useLogin() {
     mutationFn: loginApi,
     onSuccess: (user) => {
       queryClient.setQueriesData(["user"], user);
-      navigate("/dashboard");
+      navigate("/dashboard", { replace: true });
     },
     onError: (err) => {
       console.log("ERROR", err);
